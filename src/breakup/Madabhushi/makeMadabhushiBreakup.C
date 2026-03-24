6218f88f598f15a5e136441fcb5d4ebfaca2c51f
Lorenzo Proli <s336865@studenti.polito.it>
2026-03-24 20:44:33 +0100

    organizzate cartelle librerie Madabhushi

--- src/breakup/Madabhushi/makeMadabhushiBreakup.C
+++
@@ -1,8 +0,0 @@
-#include "basicSprayCloud.H" 
-#include "Madabhushi.H"
-
-// Registra semplicemente il tuo modello nella selection table esistente
-namespace Foam
-{
-    makeBreakupModelType(Madabhushi, basicSprayCloud);
-}
